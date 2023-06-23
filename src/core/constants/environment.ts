729054df4fc7d2540e63c373cc2468e5d5ae0e35
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-06-24 00:12:22 +0400

    feature: env const

---
+++ src/core/constants/environment.ts
@@ -0,0 +1,9 @@
+export const Environment = {
+  AwsAccessKey: 'AWS_ACCESS_KEY',
+  AwsSecretAccessKey: 'AWS_SECRET_ACCESS_KEY',
+  AwsSession: 'AWS_SESSION',
+
+  CognitoClientId: 'COGNITO_CLIENT_ID',
+  CognitoRegion: 'COGNITO_REGION',
+  CognitoPoolId: 'COGNITO_POOL_ID',
+} as const
